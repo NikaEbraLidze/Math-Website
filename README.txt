644b331670dd8169583f702ca4b58623b1155811
Your Name <you@example.com>
2025-01-15 13:56:02 +0400

    everithing work

--- README.txt
+++ README.txt
@@ -1,2 +1,3 @@
 I created this site to learn and master the API.
 I replaced the html file with ejs files and made the website static. After that I am going to add features.
+You can now run the site on the live server (3000). you need to use "npm init" in command line. We will connect to the server later.
--- index.js
+++ index.js
@@ -4,9 +4,25 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+const APIurl = "https://api.math.tools";
 
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+});
+
+app.get("/facts", (req, res) => {
+    res.render("numbers.ejs", { heading: "Facts about numbers", answer: "answer" });
+});
+
+app.get("/binary", (req, res) => {
+    res.render("numbers.ejs", { heading: "Convert to binary", answer: "answer" });
+});
+
+app.get("/pi", (req, res) => {
+    res.render("numbers.ejs", { heading: "Digits of PI(π)", answer: "answer" });
+});
 
 
 app.listen(port, () => {
