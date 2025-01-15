b26cc4ed815688ff3c38bc53fbee0f1233df842c
Your Name <you@example.com>
2025-01-15 13:27:16 +0400

    Remove node_modules from Git tracking

--- README.txt
+++ README.txt
@@ -1,1 +1,2 @@
 I created this site to learn and master the API.
+I replaced the html file with ejs files and made the website static. After that I am going to add features.
---
+++ index.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import bodyParser from "body-parser";
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
